4d19c667b4331f3233798335ef6a83839078872d
Sung <jajh90@gmail.com>
2022-08-09 18:45:20 -0500

    orbit controls & damping

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,17 +1,30 @@
 import './style.css'
 import * as THREE from 'three'
+import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-/**
- * Base
- */
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
     width: 800,
     height: 600
 }
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - 0.5
+    cursor.y = -(e.clientY / sizes.height - 0.5)
+})
+
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
@@ -25,11 +38,23 @@ scene.add(mesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+
+// const aspectRatio = sizes.width / sizes.height
+// console.log(aspectRatio)
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100) // left, right, top, bottom, near, far
+
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 1
+// controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -45,8 +70,17 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
 
+    // Update Camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+
+    // Update controls
+    controls.update()
+    
     // Render
     renderer.render(scene, camera)
 
