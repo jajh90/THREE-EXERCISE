c7f3e1a2d14cf24e749e7a5e7cfd88d964da9b45
Sung <jajh90@gmail.com>
2022-11-27 19:07:07 -0600

    code structuring (Exp., Sizes, Time, Camera, Renderer, World, Resources)

--- 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -1,6 +1,8 @@
 import Experience from "../Experience";
 import * as THREE from 'three'
 import Environment from "./Environment";
+import Floor from './Floor'
+import Fox from "./Fox";
 
 
 export default class World {
@@ -9,17 +11,14 @@ export default class World {
     this.scene = this.experience.scene
     this.resources = this.experience.resources
 
-    //
-    const mesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1,1,1),
-      new THREE.MeshStandardMaterial()
-    )
-    this.scene.add(mesh)
-
     this.resources.on('ready', () => {
+      this.floor = new Floor()
       this.environment = new Environment()
+      this.fox = new Fox()
     })
+  }
 
-    // Setup
+  update() {
+    if (this.fox) this.fox.update()
   }
 }
--- 26-code-structuring-for-bigger-projects/src/Experience/sources.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/sources.js
@@ -10,5 +10,20 @@ export default [
       'textures/environmentMap/pz.jpg',
       'textures/environmentMap/nz.jpg',
     ]
+  },
+  {
+    name: 'dirtColorTexture',
+    type: 'texture',
+    path: 'textures/dirt/color.jpg'
+  },
+  {
+    name: 'dirtNormalTexture',
+    type: 'texture',
+    path: 'textures/dirt/normal.jpg'
+  },
+  {
+    name: 'foxModel',
+    type: 'gltfModel',
+    path: 'models/Fox/glTF/Fox.gltf'
   }
 ]
